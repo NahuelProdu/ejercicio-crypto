7589619c3f1b27714505ea77d5f155a36e18395c
NahuelProdu <nahuel@producteca.com>
2023-01-13 18:10:24 -0300

    Add: Realizo el promedio de cada moneda

--- server/domain/mongoUtils/find.js
+++ server/domain/mongoUtils/find.js
@@ -2,6 +2,7 @@ const cryptoSchema = require("../schemas/crypto.schema");
 const config = require("../../config/environment");
 const mongoose = require("mongoose");
 const moment = require('moment');
+const _ = require('lodash');
 
 const findDataDates = (initDate, finalDate) => {
     mongoose.connect(config.mongo.uri, config.mongo.options);
@@ -11,6 +12,25 @@ const findDataDates = (initDate, finalDate) => {
             $lt: moment(finalDate).format()
         }
     })
+        .then(data => {
+            let coins = data.map(elem => elem.acronym);
+            let setCoins = new Set(coins);
+            setCoins = [...setCoins];
+            let averages = []
+            while (setCoins.length > 0) {
+                let coin = setCoins.shift();
+                let values = data.filter(elem => elem.acronym == coin);
+                let average = _.sumBy(values, value => value.rateValue) / values.length;
+                averages.push(
+                    {
+                        acronym: coin,
+                        average: average
+                    }
+                )
+            }
+
+            return averages;
+        })
 }
 
 const findDataPagination = (cripto, page, limit = 5) => {
