230d62c519ea18928aaa621973eb9bb14763d0ef
Nahuel Farias <nahuel@producteca.com>
2023-02-24 10:16:42 -0300

    Fix: Elimino comentario

--- server/api/coins/index.js
+++ server/api/coins/index.js
@@ -2,7 +2,6 @@ var express = require("express");
 var controller = require("./coin.controller");
 var router = express.Router();
 
-//TODO: Cambiar nombre de register - Poner la moneda en la URL
 router.get("/", controller.data);
 router.get("/:average", controller.average);
 router.delete("/:acronym", controller.delete);
